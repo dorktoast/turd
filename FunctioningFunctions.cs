616630f145e3edd7ec22176245bf0b92ed5ae7db
DorkToast <sam@gib.games>
2023-08-22 05:29:17 -0600

    now with 115% more function

---
+++ FunctioningFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class FunctioningFunctions
+{
+	public void Procrastinate()
+	{
+		object a = new object();
+		
+		// TODO
+	}
+}
+
+// dorktoast
