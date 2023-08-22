6677c9380f75acc3985db390abcd570765cb3ef1
DorkToast <sam@gib.games>
2023-08-22 08:00:46 -0600

    now less functional

--- FunctioningFunctions.cs
+++ FunctioningFunctions.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 
 public class FunctioningFunctions
 {
+	private Joke joke;
+	
+	/// <summary>
+	/// It's ok, you wouldn't get it.
+	/// </summary>
+	public void SetJoke(Joke newJoke)
+	{
+		this.joke = newJoke;
+	}
+	
 	public void Procrastinate()
 	{
 		object a = new object();
