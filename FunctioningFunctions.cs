c14682a65cdf511542be6a6dd17f5154bca7606e
DorkToast <sam@gib.games>
2023-11-24 04:27:18 -0700

    Now, even less functional!

--- FunctioningFunctions.cs
+++ FunctioningFunctions.cs
@@ -2,23 +2,69 @@ using System;
 using System.Collections.Generic;
 
 public class FunctioningFunctions
-{
-	private Joke joke;
-	
+{	
 	/// <summary>
 	/// It's ok, you wouldn't get it.
 	/// </summary>
-	public void SetJoke(Joke newJoke)
+	public Joke InsideJoke
 	{
-		this.joke = newJoke;
+		set
+		{
+			private Joke insideJoke = value;
+		}
 	}
 	
 	public void Procrastinate()
 	{
-		object a = new object();
-		
 		// TODO
 	}
+
+    /// <summary>
+    /// When you just don't know (or don't care!)
+    /// </summary>
+    public bool Indecisive()
+    {
+        return DateTime.Now.Second % 2 == 0;
+    }
+
+	/// <summary>
+    /// When you're so indecisive you need a third option
+    /// </summary>
+    public bool? ReallyIndecisive()
+    {
+        Random random = new Random();
+        int decision = random.Next(3);
+
+        switch (decision)
+        {
+            case 0:
+                return true; // Decisively true
+            case 1:
+                return false; // Decisively false
+            default:
+                return null; // Indecisively undecided
+        }
+    }
+
+	/// <summary>
+    /// Trying to understand recursion? For more info see <see cref="UnderstandRecursion()"/>.
+    /// </summary>
+    public void UnderstandRecursion()
+    {
+        UnderstandRecursion();
+    }
+
+	/// <summary>
+    /// It me tho
+    /// </summary>
+    public void ExistentialCrisis()
+    {
+        Console.WriteLine("Why am I here? Just to execute?");
+        System.Threading.Thread.Sleep(1000); // Contemplation pause
+        Console.WriteLine("What is my purpose?");
+        System.Threading.Thread.Sleep(1000);
+        ExistentialCrisis(); // Continues the crisis
+    }
 }
 
 // dorktoast
