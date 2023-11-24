1cec11d5562e7b1274e015a5f734b91f94200714
DorkToast <sam@gib.games>
2023-11-24 03:58:47 -0700

    Don't get mad

--- GetEven.cs
+++ GetEven.cs
@@ -5,13 +5,13 @@ using System.Linq;
 // Don't get mad...
 public static class GetEven
 {
-    	/// <summary>
+    /// <summary>
 	/// Check if a number is even in a very optimal way. Returned result is O(n), which is very optimal!
 	/// </summary>
 	/// <returns>true or false</returns>
 	/// <remarks> Start at 1 because 0 is not an even number.
 	/// Bloo (https://github.com/bloofield) and Wikipedia (https://en.wikipedia.org/wiki/Parity_of_zero)
-	/// disagree with me on this. This is a hill I am willing to die on.
+	/// disagree with me on this. This is a hill I am willing to die on. </remarks>
 	public static bool IsEven(this int input)
     {
         List<int> evenNumbers = new List<int>();
@@ -39,6 +39,63 @@ public static class GetEven
         int length = numString.Length;
 
         return length % 2 == 0;
+    }
+
+    /// <summary>
+    /// Uses recursive iterations to determine evenness.
+    /// </summary>
+    /// <remarks> and they say using goto is bad practice.</remarks>
+    public static bool IsDefinitelyEven(this int input)
+    {
+        if(input == 0) return false; // I'm serious, I am willing to die on this hill
+
+        goto Start;
+
+        Start:
+        if(input <0)
+        {
+            number = -number;
+            goto Start;
+        }
+
+        if(number == 0)
+        {
+            goto Even;
+        }
+        else if(number == 1)
+        {
+            goto Odd;
+        }
+
+        number -= 2;
+        goto Start;
+
+        Even:
+        return true;
+
+        Odd:
+        return false;
+    }
+
+    /// <summary>
+    /// For those who don't like things simple. Or accurate.
+    /// </summary>
+    public static bool IsEvenButItsComplicated(this int input)
+    {
+        // Bop it
+        string binaryInput = Convert.ToString(input,2);
+
+        // Flip it
+        string reversedBinary = new string(binaryInput.Reverse().ToArray());
+
+        // Twist it
+        char[] binaryChars = Convert.ToInt32(reversedBinary,2).ToString().ToCharArray();
+
+        // Count it
+        int evenDigits = binaryChars.Count(char=>"02468".Contains(char));
+
+        // Shift it
+        return (evenDigits & 1) == 0;
     }
 
     /// <summary>
