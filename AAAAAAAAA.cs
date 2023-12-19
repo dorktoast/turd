242deae10ed480df7f5651f3b3d0d8c31b9d3af8
DorkToast <sam@gib.games>
2023-12-19 06:14:45 -0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ AAAAAAAAA.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AAAAAAAAAA;
+
+namespace AAAAAAAAAAAAAAAA.AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+{
+    /// <summary>
+    /// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    /// </summary>
+    /// <remarks>AAAAAAAAAAAAAAAAAAAAAAAAAA</remarks>
+    public static class AAAAAAAAAAAAAAAA : AAAAAAAAAAAAAA
+    {
+        public const AAAAAAAAAAAAAAAAAAA AAAAAAAAAAAA = AAAAAAAAAAAAAAAAAAA.AAAAAAAA;
+        public int AAAAAAAAAAAAAAAAAAAAAA;
+        public static AAAAAAAAAAAAAAAA _AAAAAAAAAAAAAAAA;
+        [SerializeField] private AAAAAAAAAAAAAAAA aAAAAAAAAAAAAAAAAAAA;
+
+        private AAAAAAAAAAAAAAAA aAAAAAAAAAAAAAAAA;
+        public AAAAAAAAAAAAAAAA { get => aAAAAAAAAAAAAAAAA; private set { } }
+
+        public void AAAAAAAAAAAAAAAAAAAAA(AAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAA)
+        {
+            AAAAAAAAAAAAAAAAA();
+            Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        }
+
+        private void AAAAAAAAAAAAAAAAA()
+        {
+            switch (AAAAAAAAAAAA)
+            {
+                case A:
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAA();
+                    break;
+                case AA:
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAA();
+                    break;
+                case AAA:
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAA();
+                    break;
+                case AAAA:
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAA();
+                    break;
+                case AAAAA:
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAA();
+                    break;
+                case AAAAAA:
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAA();
+                    break;
+                case AAAAAAA:
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAA();
+                    break;
+                case AAAAAAAA:
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAA();
+                    break;
+                case AAAAAAAAA:
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAA();
+                    break;
+            }
+        }
+
+        private void AAAAAAAAAAAAAAAAAAAAAAAAAAA()
+        {
+            AAAAAAAAAAAAAAAAAAAAAA++;
+        }
+    }
+
+    public enum AAAAAAAAAAAAAAAAAAA
+    {
+        A,
+        AA,
+        AAA,
+        AAAA,
+        AAAAA,
+        AAAAAA,
+        AAAAAA,
+        AAAAAAA,
+        AAAAAAAA
+    }
+}
